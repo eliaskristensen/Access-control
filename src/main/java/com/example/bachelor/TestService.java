f68aea550dc0396136e5eb1586a7105a499c2bbc
jesp0 <jesprkraft@gmail.com>
2024-03-18 11:28:16 +0100

    Test for getting data from the informationmodel

--- src/main/java/com/example/bachelor/TestService.java
+++ src/main/java/com/example/bachelor/TestService.java
@@ -7,18 +7,26 @@ import org.reflections.Reflections;
 import org.springframework.stereotype.Service;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Set;
+
+import static java.util.Collections.sort;
 
 @Service
 @Slf4j
 public class TestService {
     @PostConstruct
     public void test(){
+        ArrayList<String> testliste = new ArrayList<>();
         Set<Class<? extends FintMainObject>> subTypesOf = new Reflections("no.fint.model").getSubTypesOf(FintMainObject.class);
         subTypesOf.forEach(clazz -> {
             // Tester at vi får tak i informasjonsmodellen
-            System.out.println(clazz.getSimpleName());
-
+            testliste.add(clazz.getSimpleName());
+            sort(testliste);
+            //System.out.println(clazz.getSimpleName());
         });
+        System.out.println(testliste);
+        System.out.println(testliste.size() + "");
     }
 }
